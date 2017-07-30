7adc3747a43d6b2ffd816615ec8244360a8527ba
Chad Cook <chadlcook3@gmail.com>
2017-07-29 18:17:37 -0700

    update

---
+++ controllers/burgerController.js
@@ -0,0 +1,50 @@
+var express = require("express");
+
+var router = express.Router();
+
+
+var cat = require("../models/burger.js");
+
+
+router.get("/", function(req, res) {
+  cat.all(function(data) {
+    var hbsObject = {
+      cats: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/", function(req, res) {
+  cat.create([
+    "name", "hungary"
+  ], [
+    req.body.name, req.body.hungary
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+router.put("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    hungary: req.body.hungary
+  }, condition, function() {
+    res.redirect("/");
+  });
+});
+
+router.delete("/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function() {
+    res.redirect("/");
+  });
+});
+
+
+module.exports = router;
